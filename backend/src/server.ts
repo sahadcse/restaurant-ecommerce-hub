31444167b3795c256dbfe0543a46a8d6ab825771
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2025-04-04 10:38:06 +0600

    Day 7: Setup Next.js frontend with customer homepage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,15 +4,26 @@ import pool from './db';
 import authRouter from './routes/auth.route';
 import restaurantRouter from './routes/restaurants.route';
 import menuRouter from './routes/menu.route';
+import orderRouter from './routes/orders.route';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const port: number = Number(process.env.PORT) || 3001;
 
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: '*', // Allow requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 app.use('/auth', authRouter);
 app.use('/restaurants', restaurantRouter);
 app.use('/menu', menuRouter);
+app.use('/orders', orderRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Restaurant E-Commerce Hub Backend (TypeScript)');
